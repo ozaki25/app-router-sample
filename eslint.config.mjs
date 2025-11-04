cb656b97ef9cccc4e99f85f54763ebd3288f5368
Yuichi Ozaki <yuichi_ozaki@kokuyo.com>
2025-11-05 01:29:33 +0900

    add storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,5 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -14,6 +16,7 @@ const eslintConfig = [
   {
     ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
   },
+  ...storybook.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
