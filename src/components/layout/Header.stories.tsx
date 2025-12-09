c67fa7c7fa1626133ff06552048c3a9e497d6b4b
ozaki25 <fs65ugumori@yahoo.co.jp>
2025-12-10 01:17:02 +0900

    story追加

---
+++ src/components/layout/Header.stories.tsx
@@ -0,0 +1,17 @@
+import { Header } from './Header';
+import type { Meta, StoryObj } from '@storybook/nextjs';
+
+const meta = {
+  title: 'Layout/Header',
+  component: Header,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'デフォルト',
+};
