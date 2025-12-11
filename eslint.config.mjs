a4cea768d965626f657f5be3e4988b89ed0c4715
Yuichi Ozaki <yuichi_ozaki@kokuyo.com>
2025-12-11 16:14:58 +0900

    eslint configをリファクタ

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,24 @@
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import storybook from 'eslint-plugin-storybook';
-import { defineConfig, globalIgnores } from 'eslint/config';
 
-const eslintConfig = defineConfig([
+const eslintConfig = [
   ...nextVitals,
   ...nextTs,
   ...storybook.configs['flat/recommended'],
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-    '.storybook/public/mockServiceWorker.js',
-    'storybook-static/**',
-    '**/*mock*/**',
-    'coverage/**',
-  ]),
-]);
+  {
+    ignores: [
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
+      '.storybook/public/mockServiceWorker.js',
+      'storybook-static/**',
+      '**/*mock*/**',
+      'coverage/**',
+      '!.storybook',
+    ],
+  },
+];
 
 export default eslintConfig;
